c4678efaf1e0410984b315be7a0bd4661ffe0345
Fillonit Ibishi <filloniti@gmail.com>
2022-12-22 14:48:32 +0100

    asd

--- login.js
+++ login.js
@@ -1,3 +1,11 @@
 function exitLogin() {
     window.location.href = "/";
 }
+
+function inputFocus() {
+    console.log("Typing...");
+}
+
+document.querySelector("input[type=password]").addEventListener('focus', () => {
+    console.log("Entering password");
+});
