e7bdeaac52f216fe4e71c990413c854e4c5af0e1
Fillonit Ibishi <filloniti@gmail.com>
2023-02-03 21:07:30 +0100

    using post instead of request

--- api/login.php
+++ api/login.php
@@ -1,8 +1,8 @@
 <?php 
     include 'model.php';
     $model = new Model();
-    $username = $_REQUEST['username'];
-    $password = $_REQUEST['password'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
     $login = $model->login($username, $password);
  
     if (!$login) {
