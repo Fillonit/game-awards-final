d5307a79375f25ef12d8f6c447d5d846e8b64316
Fillonit Ibishi <filloniti@gmail.com>
2023-02-04 14:17:07 +0100

    asd

--- api/dashboard.php
+++ api/dashboard.php
@@ -2,9 +2,16 @@
 
 // start a session to store user data
 session_start();
+session_start();
+
+// Check if the user is logged in and has isAdmin = 1
+if (!isset($_SESSION['user']) || !isset($_SESSION['user']['isAdmin']) || $_SESSION['user']['isAdmin'] != 1) {
+    header('Location: login.php');
+    exit;
+}
 
 // check if user is not logged in
-if(!isset($_SESSION['user_id'])){
+if(!isset($_SESSION['user'])){
     header("Location: log_in.php");
 }
 
@@ -13,7 +20,7 @@ include_once 'model.php';
 $model = new Model();
 
 // get user data
-$user = $model->getUser($_SESSION['user_id']);
+$user = $model->getUser($_SESSION['user']['username'], $_SESSION['user']['password']);
 
 ?>
 
--- api/model.php
+++ api/model.php
@@ -97,5 +97,14 @@
                 echo `Incorrect username or password.`;
             }
         }
+
+        public function getUser($username, $password){
+            $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+            $result = $this->conn->query($query);
+            if ($result->num_rows > 0) {
+                return $result->fetch_assoc();
+            }
+            return null;
+        }
     }
 ?>
