09870a3b743f9279580f02b9107e66c4233d62e0
Fillonit Ibishi <filloniti@gmail.com>
2023-02-04 14:24:14 +0100

    asd

--- api/log_in.php
+++ api/log_in.php
@@ -25,27 +25,3 @@
         }
     }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Login</title>
-</head>
-<body>
-    <h1>Login</h1>
-    <?php
-        if(isset($error)){
-            echo "<p style='color:red'>".$error."</p>";
-        }
-    ?>
-    <form action="" method="post">
-        <label for="username">Username:</label>
-        <input type="text" name="username" required>
-
-        <label for="password">Password:</label>
-        <input type="password" name="password" required>
-
-        <input type="submit" name="submit" value="Login">
-    </form>
-</body>
-</html>
