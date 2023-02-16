d6f44adbbf99c724df432a4c5cb3d429687a5844
Fillonit Ibishi <filloniti@gmail.com>
2023-02-16 19:33:08 +0100

    file structure fix

--- api/pages/game.php
+++ api/pages/game.php
@@ -35,25 +35,25 @@ session_start();
 
     <link rel="shortcut icon" href="assets/img/nocticon.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="../style.css">
-    <link rel="stylesheet" href="../gameDetail.css">
-    <script src="../script.js" defer></script>
+    <link rel="stylesheet" href="../../style.css">
+    <link rel="stylesheet" href="../../gameDetail.css">
+    <script src="../../script.js" defer></script>
 </head>
 
 <body>
     <div class="header">
         <!-- <img src="" alt="" class="logo"> -->
         <!-- <a href="/" class="logo"><h1 class="logo">Nocturne</h1></a> -->
-        <a href="/" class="logo"><img src="../assets/img/noctlogo1.png" alt="Nocturne"></a>
+        <a href="/" class="logo"><img src="../../assets/img/noctlogo1.png" alt="Nocturne"></a>
         <div class="nav">
             <a href="/">Home</a>
             <a href="/about.html">About</a>
             <a href="/contact.html">Contact</a>
             <a href="/games.html">Games</a>
-            <a href="/api/dashboard.php" id="dashboardURL">Dashboard</a>
+            <a href="/api/admin/dashboard.php" id="dashboardURL">Dashboard</a>
         </div>
 
-        <a href="/api/login.php"><button class="LoginBtn" id="LoginBtn"><span id="loggedInUser">Login</span> <i class="fa-solid fa-arrow-right-to-bracket"></i></button></a>
+        <a href="/api/user/login.php"><button class="LoginBtn" id="LoginBtn"><span id="loggedInUser">Login</span> <i class="fa-solid fa-arrow-right-to-bracket"></i></button></a>
     </div>
     </div>
     <main>
