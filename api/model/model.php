899740565d386fd3b16f92d13933e482797572b2
Fillonit Ibishi <filloniti@gmail.com>
2023-02-17 17:48:15 +0100

    asd

--- api/model/model.php
+++ api/model/model.php
@@ -1,154 +1,171 @@
 <?php
-    class Model{
-        private $server = 'localhost';
-        private $username = 'root';
-        private $password = '';
-        private $database = 'gameawards';
-        private $conn;
+class Model
+{
+    private $server = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $database = 'gameawards';
+    private $conn;
 
-        public function __construct(){
-            try{
-                $this->conn = new mysqli($this->server, $this->username, $this->password, $this->database);
-            }catch(Exception $ex){
-                echo 'connection failed' .$ex->getMessage();
-            }       
+    public function __construct()
+    {
+        try {
+            $this->conn = new mysqli($this->server, $this->username, $this->password, $this->database);
+        } catch (Exception $ex) {
+            echo 'connection failed' . $ex->getMessage();
         }
+    }
 
-        //INSERT, FETCH, EDIT, DELETE
+    //INSERT, FETCH, EDIT, DELETE
 
-        public function insert(){
-            // if(isset($_POST['submit'])){
+    public function insert()
+    {
+        // if(isset($_POST['submit'])){
 
-                $username = $_POST['username'];
-                $password = $_POST['password'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
-                $query = "INSERT INTO users(username, password) VALUES ('$username', '$password')";
-                if ($sql = $this->conn->query($query)) {
-                    echo "<script>alert('records added successfully');</script>";
-                    echo "<script>window.location.href = '/';</script>";
-                }else{
-                    echo "<script>alert('failed');</script>";
-                    echo "<script>window.location.href = 'login.php';</script>";
-                }
-            // }
+        $query = "INSERT INTO users(username, password) VALUES ('$username', '$password')";
+        if ($sql = $this->conn->query($query)) {
+            echo "<script>alert('records added successfully');</script>";
+            echo "<script>window.location.href = '/';</script>";
+        } else {
+            echo "<script>alert('failed');</script>";
+            echo "<script>window.location.href = 'login.php';</script>";
         }
+        // }
+    }
 
-        
-        public function fetch(){
-            $data = null;
-            $query = "SELECT * FROM users";
-            if ($sql = $this->conn->query($query)) {
-                while ($row = mysqli_fetch_assoc($sql)) {
-                    $data[] = $row;
-                }
-            }
-            return $data;
-        }
- 
-        public function delete($id){
- 
-            $query = "DELETE FROM users where id = '$id'";
-            if ($sql = $this->conn->query($query)) {
-                return true;
-            }else{
-                return false;
+
+    public function fetch()
+    {
+        $data = null;
+        $query = "SELECT * FROM users";
+        if ($sql = $this->conn->query($query)) {
+            while ($row = mysqli_fetch_assoc($sql)) {
+                $data[] = $row;
             }
         }
- 
-        public function edit($id){
- 
-            $data = null;
- 
-            $query = "SELECT * FROM users WHERE id = '$id'";
-            if ($sql = $this->conn->query($query)) {
-                while($row = $sql->fetch_assoc()){
-                    $data = $row;
-                }
-            }
-            return $data;
-        }
- 
-        public function update($data){
- 
-            $query = "UPDATE users SET id='$data[id]', username='$data[username]', password='$data[password]', isAdmin='$data[isAdmin]' WHERE users.id='$data[id]'";
- 
-            if ($sql = $this->conn->query($query)) {
-                return true;
-            }else{
-                return false;
-            }
-        }
+        return $data;
+    }
 
-        public function login($username, $password){
- 
-            $data = null;
- 
-            $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-            if ($sql = $this->conn->query($query)) {
-                while($row = $sql->fetch_assoc()){
-                    $data = $row;
-                }
+    public function delete($id)
+    {
+
+        $query = "DELETE FROM users where id = '$id'";
+        if ($sql = $this->conn->query($query)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function edit($id)
+    {
+
+        $data = null;
+
+        $query = "SELECT * FROM users WHERE id = '$id'";
+        if ($sql = $this->conn->query($query)) {
+            while ($row = $sql->fetch_assoc()) {
+                $data = $row;
             }
-            if($data != null) {
-            return(json_encode($data));
+        }
+        return $data;
+    }
+
+    public function update($data)
+    {
+
+        $query = "UPDATE users SET id='$data[id]', username='$data[username]', password='$data[password]', isAdmin='$data[isAdmin]' WHERE users.id='$data[id]'";
+
+        if ($sql = $this->conn->query($query)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function login($username, $password)
+    {
+
+        $data = null;
+
+        $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+        if ($sql = $this->conn->query($query)) {
+            while ($row = $sql->fetch_assoc()) {
+                $data = $row;
+            }
+        }
+        if ($data != null) {
+            return (json_encode($data));
+        } else {
+            return `Incorrect username or password.`;
+        }
+    }
+
+    public function getUser($username, $password)
+    {
+        $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+        $result = $this->conn->query($query);
+        if ($result->num_rows > 0) {
+            return $result->fetch_assoc();
+        }
+        return null;
+    }
+
+    public function addUser()
+    {
+        if (isset($_POST['submit'])) {
+
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $isAdmin = $_POST['isAdmin'];
+
+            $query = "INSERT INTO users(username, password, isAdmin) VALUES ('$username','$password', '$isAdmin')";
+            if ($sql = $this->conn->query($query)) {
+                echo "<script>alert('records added successfully');</script>";
+                echo "<script>window.location.href = 'dashboard.php';</script>";
             } else {
-                return `Incorrect username or password.`;
-            }
-        }
-
-        public function getUser($username, $password){
-            $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-            $result = $this->conn->query($query);
-            if ($result->num_rows > 0) {
-                return $result->fetch_assoc();
-            }
-            return null;
-        }
-
-        public function addUser(){
-            if(isset($_POST['submit'])){
-
-                $username = $_POST['username'];
-                $password = $_POST['password']; 
-                $isAdmin = $_POST['isAdmin']; 
-
-                $query = "INSERT INTO users(username, password, isAdmin) VALUES ('$username','$password', '$isAdmin')";
-                if ($sql = $this->conn->query($query)) {
-                    echo "<script>alert('records added successfully');</script>";
-                    echo "<script>window.location.href = 'dashboard.php';</script>";
-                }else{
-                    echo "<script>alert('failed');</script>";
-                    echo "<script>window.location.href = 'login.php';</script>";
-                }
-            }
-        }
-
-        public function getGames(){
-            $data = null;
-            $query = "SELECT * FROM games";
-            if ($sql = $this->conn->query($query)) {
-                while ($row = mysqli_fetch_assoc($sql)) {
-                    $data[] = $row;
-                }
-            }
-            return $data;
-        }
-
-        public function contactMail(){
-            if ($_SERVER["REQUEST_METHOD"] === "POST"){
-
-                $name = $_POST['name'];
-                $email = $_POST['email'];
-                $message = $_POST['message'];
-
-                $query = "INSERT INTO contact(name, email, message) VALUES ('$name', '$email', '$message')";
-                if ($sql = $this->conn->query($query)) {
-                    echo "<script>alert('Contact Message has been sent successfully');</script>";
-                    echo "<script>window.location.href = '/api/pages/index.php';</script>";
-                }else{
-                    echo "<script>alert('failed');</script>";
-                    echo "<script>window.location.href = '/api/user/login.php';</script>";
-                }
+                echo "<script>alert('failed');</script>";
+                echo "<script>window.location.href = 'login.php';</script>";
             }
         }
     }
+
+    public function getGames()
+    {
+        $data = null;
+        $query = "SELECT * FROM games";
+        if ($sql = $this->conn->query($query)) {
+            while ($row = mysqli_fetch_assoc($sql)) {
+                $data[] = $row;
+            }
+        }
+        return $data;
+    }
+
+    public function contactMail()
+    {
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $message = $_POST['message'];
+
+            if (empty($name) || empty($message) || empty($email)) {
+                echo "Please fill all the fields.";
+                exit();
+            }
+
+            $query = "INSERT INTO contact(name, email, message) VALUES ('$name', '$email', '$message')";
+            if ($sql = $this->conn->query($query)) {
+                echo "<script>alert('Contact Message has been sent successfully');</script>";
+                echo "<script>window.location.href = '/api/pages/index.php';</script>";
+            } else {
+                echo "<script>alert('failed');</script>";
+                echo "<script>window.location.href = '/api/user/login.php';</script>";
+            }
+        }
+    }
+}
--- api/pages/contact.php
+++ api/pages/contact.php
@@ -35,18 +35,18 @@
         $model = new Model();
         $insert = $model->contactMail();
         ?>
-    <form onsubmit="validate();" method="POST" action="#">
+    <form onsubmit="validate();" method="POST" action="contact.php">
         <div class="form-group">
             <label for="name">Name:</label>
-            <input type="text" class="form-control" id="name" placeholder="Enter your name">
+            <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
         </div>
         <div class="form-group">
             <label for="email">Email:</label>
-            <input type="email" class="form-control" id="email" placeholder="Enter your email">
+            <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
         </div>
         <div class="form-group">
             <label for="message">Message:</label>
-            <textarea class="form-control" id="message" rows="5" placeholder="Enter your message" id="message"></textarea>
+            <textarea class="form-control" name="message" rows="5" placeholder="Enter your message" id="message"></textarea>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
