80325624d0e46c57c876be79eddca639ad1b6e32
Fillonit Ibishi <filloniti@gmail.com>
2023-02-19 12:00:38 +0100

    TOS

--- api/pages/TOS.php
+++ api/pages/TOS.php
@@ -44,7 +44,7 @@
     </div>
 
     <body>
-
+        <!-- 
         <body>
             <h1>Terms of Use</h1>
             <p>Welcome to our website. If you continue to browse and use this website, you are agreeing to comply with
@@ -71,6 +71,121 @@
             <h2>Your use of this website and any dispute arising out of such use of the website is subject to the laws
                 of Kosovo.</h2>
 
+        </body> -->
+
+        <style>
+            main {
+                font-family: Arial, sans-serif;
+                font-size: 16px;
+                line-height: 1.5;
+                padding: 30px;
+            }
+
+            h1,
+            h2 {
+                font-weight: bold;
+                margin-bottom: 20px;
+            }
+
+            h1 {
+                font-size: 28px;
+            }
+
+            h2 {
+                font-size: 24px;
+            }
+
+            p,
+            ul {
+                margin-bottom: 20px;
+            }
+
+            ul {
+                list-style-type: square;
+                margin-left: 20px;
+            }
+
+            li {
+                margin-bottom: 5px;
+            }
+
+            table {
+                border-collapse: collapse;
+                margin-bottom: 20px;
+                width: 100%;
+            }
+
+            th,
+            td {
+                border: 1px solid #ddd;
+                padding: 10px;
+                text-align: left;
+            }
+
+            th {
+                background-color: #f2f2f2;
+            }
+        </style>
+        </head>
+
+        <body>
+            <main>
+            <h1>Terms of Use</h1>
+
+            <h2>1. Introduction</h2>
+            <p>These Website Standard Terms of Service (these “Terms” or these “Website Standard Terms of Service”)
+                contained herein on this webpage, shall govern your use of this website, including all pages within this
+                website (collectively referred to herein below as this “Website”). These Terms apply in full force and
+                effect to your use of this Website and by using this Website, you expressly accept all terms and
+                conditions contained herein in full. If you disagree with these Terms or any part of these Terms, you
+                must not use this Website.</p>
+
+            <h2>2. Intellectual Property Rights</h2>
+            <p>Unless otherwise stated, we or our licensors own the intellectual property rights in the website and
+                material on the website. Subject to the license below, all these intellectual property rights are
+                reserved.</p>
+
+            <h2>3. License to Use Website</h2>
+            <p>You may view, download for caching purposes only, and print pages from the website for your own personal
+                use, subject to the restrictions set out below and elsewhere in these Terms of Service.</p>
+
+            <h2>4. Acceptable Use</h2>
+            <p>You must not use this Website in any way that causes, or may cause, damage to the Website or impairment
+                of the availability or accessibility of the Website, or in any way which is unlawful, illegal,
+                fraudulent or harmful, or in connection with any unlawful, illegal, fraudulent or harmful purpose or
+                activity.</p>
+            <ul>
+                <li>You must not conduct any systematic or automated data collection activities (including without
+                    limitation scraping, data mining, data extraction and data harvesting) on or in relation to this
+                    Website without our express written consent.</li>
+                <li>You must not use this Website to transmit or send unsolicited commercial communications.</li>
+                <li>You must not use this Website for any purposes related to marketing without our express written
+                    consent.</li>
+            </ul>
+
+            <h2>5. Limitations of Liability</h2>
+            <p>We will not be liable to you (whether under the law of contract, the law of torts or otherwise) in
+                relation to the contents of, or use of, or otherwise in connection with, this Website for any direct,
+                indirect or consequential loss; or for any business losses, loss of revenue, income, profits or
+                anticipated savings, loss of contracts or business relationships
+                </main>
+                </div>
+                </section>
+            <footer>
+                <div class="footer-container">
+                    <p>&copy; <?php echo date('Y'); ?> MyWebsite</p>
+                    <nav>
+                        <ul>
+                            <li><a href="#">Terms of Use</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="https://github.com/myusername"><i class="fab fa-github"></i></a></li>
+                            <li><a href="https://www.linkedin.com/in/myusername/"><i class="fab fa-linkedin"></i></a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </footer>
         </body>
 
 </html>
